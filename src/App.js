a169afe3420360ee8c67df2b1db23d58493e7026
Andre Masson <amwebexpert@gmail.com>
2021-04-16 16:19:46 -0400

    Upgraded packages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
-import { ReactQueryDevtools } from "react-query-devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   let [page, setPage] = useState("planets");
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="App">
         <h1>Star Wars Info</h1>
         <Navbar setPage={setPage} />
@@ -16,8 +18,7 @@ function App() {
           {page === "planets" ? <Planets /> : <People />}
         </div>
       </div>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </>
+    </QueryClientProvider>
   );
 }
 
